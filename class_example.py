92bcf92552e0e1bcad85490a88795e767c11dc2d
Serrones <fabioserrones@gmail.com>
2019-08-25 15:22:23 -0300

    Class Pizza

--- class_example.py
+++ class_example.py
@@ -1,3 +1,6 @@
+import math
+
+
 class MyClass:
     def method(self):
         return 'instance method called', self
@@ -9,3 +12,27 @@ class MyClass:
     @staticmethod
     def static_method():
         return 'static method called'
+
+class Pizza:
+    def __init__(self, radius, ingredients):
+        self.radius = radius
+        self.ingredients = ingredients
+
+    def __repr__(self):
+        return (f'Pizza({self.radius}, '
+                f'{self.ingredients})')
+
+    @classmethod
+    def margherita(cls):
+        return cls(['mozzarella', 'tomatoes'])
+
+    @classmethod
+    def prosciutto(cls):
+        return cls(['mozzarella', 'tomatoes', 'ham'])
+
+    def area(self):
+        return self.circle_area(self.radius)
+
+    @staticmethod
+    def circle_area(r):
+        return r**2 * math.pi
